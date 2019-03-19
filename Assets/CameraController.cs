d5aeb5caa77b72aef76802cb487cb25d952a9dcd
valkyrienyanko <sebastianbelle074@gmail.com>
2019-03-18 19:51:24 -0500

    Commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+    public float speed = 1.0f;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        float interpolation = speed * Time.deltaTime;
+
+        Vector3 position = transform.position;
+        position.x = Mathf.Lerp(transform.position.x, player.transform.position.x, interpolation);
+        position.y = Mathf.Lerp(transform.position.y, player.transform.position.y, interpolation);
+
+        transform.position = position;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float maxSpeed = 1f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorz = Input.GetAxis("Horizontal");
+        float moveVert = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorz, moveVert);
+
+        rb.AddForce(movement, ForceMode2D.Impulse);
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+    }
+}
