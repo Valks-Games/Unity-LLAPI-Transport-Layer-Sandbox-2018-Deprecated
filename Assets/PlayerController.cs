fa61918ae05f44305164aba8856a73082a69cb38
valkyrienyanko <sebastianbelle074@gmail.com>
2019-03-18 21:45:44 -0500

    Water Physics

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,13 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
 {
-    public float maxSpeed = 1f;
+    public GameObject gridGameObject;
+    public GameObject tilemapGameObject;
+
+    public float maxSpeed = 25f;
+    public float speed = 500f;
+
     private Rigidbody2D rb;
+    private Grid grid;
+    private Tilemap tilemap;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        grid = gridGameObject.GetComponent<Grid>();
+        tilemap = tilemapGameObject.GetComponent<Tilemap>();
     }
 
     void FixedUpdate()
@@ -15,9 +25,21 @@ public class PlayerController : MonoBehaviour
         float moveHorz = Input.GetAxis("Horizontal");
         float moveVert = Input.GetAxis("Vertical");
 
-        Vector2 movement = new Vector2(moveHorz, moveVert);
+        Vector2 movement = new Vector2(moveHorz, moveVert) * Time.deltaTime * speed;
 
-        rb.AddForce(movement, ForceMode2D.Impulse);
-        rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+        rb.AddForce(movement, ForceMode2D.Force);
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed * Time.deltaTime);
+
+        Vector3Int pos = grid.WorldToCell(transform.position);
+
+        string name = tilemap.GetTile(pos).name;
+
+        if (name.Equals("Water"))
+        {
+            speed = 100f;
+        }
+        else {
+            speed = 500f;
+        }
     }
 }
